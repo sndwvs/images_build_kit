3718242cb2d1ae88fb864e266dda724362865380
mara <mara@fail.pp.ua>
2017-02-22 21:53:13 +0200

    change configuration next in legacy rk3288

--- configuration.sh
+++ configuration.sh
@@ -22,15 +22,15 @@ BOOT_LOADER_BRANCH="" #>v2016.07
 # xtools configuration
 #---------------------------------------------
 XTOOLS="x-tools7h"
-if [[ $SOCFAMILY == sun* ]] && [[ $KERNEL_SOURCE != next ]]; then
+if [[ $KERNEL_SOURCE != next ]]; then
     VERSION_XTOOLS="5.3.0-5"
     MD5_XTOOLS="43562de45d89d1d7de9193c44b2e6909"
-elif [[ $SOCFAMILY == rk3288 ]]; then
-    VERSION_XTOOLS="4.9.2-4"
-    MD5_XTOOLS="ccbfa040c1949dad6d32505fa9d973b9"
+#elif [[ $SOCFAMILY == rk3288 ]]; then
+#    VERSION_XTOOLS="4.9.2-4"
+#    MD5_XTOOLS="ccbfa040c1949dad6d32505fa9d973b9"
 else
-    VERSION_XTOOLS="6.1.1-4"
-    MD5_XTOOLS="2e78ef5e1241bfb8e35e2fc3132d68f1"
+    VERSION_XTOOLS="6.2.1-1"
+    MD5_XTOOLS="58766cfb988135f761d9f4c2c0d5c4e4"
 fi
 URL_XTOOLS="https://archlinuxarm.org/builder/xtools/$VERSION_XTOOLS/$XTOOLS.tar.xz"
 
