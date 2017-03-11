5f45869b742b9dd4fe4fca69c2d3df2789da14a0
mara <mara@fail.pp.ua>
2017-03-11 02:09:51 +0200

    fix boot H3 add parameter machid 1029

--- config/boot_scripts/boot-sunxi.cmd
+++ config/boot_scripts/boot-sunxi.cmd
@@ -19,12 +19,12 @@ itest.b *0x28 == 0x03 && echo "U-boot loaded from SPI"
 
 echo "Boot script loaded from ${devtype}"
 
-if load ${devtype} 0 ${load_addr} /boot/uEnv.txt || load ${devtype} 0 ${load_addr} uEnv.txt; then
+if load ${devtype} 0 ${load_addr} /boot/uEnv.txt; then
 	env import -t ${load_addr} ${filesize}
 fi
 
-if test "${console}" = "display" || test "${console}" = "both"; then setenv consoleargs "console=tty1"; fi
-if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "${consoleargs} console=ttyS0,115200"; fi
+if test "${console}" = "display" || test "${console}" = "both"; then setenv consoleargs "console=ttyS0,115200"; fi
+if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "${consoleargs} earlyprintk console=tty1"; fi
 
 # get PARTUUID of first partition on SD/eMMC it was loaded from
 # mmc 0 is always mapped to device u-boot (2016.09+) was loaded from
@@ -34,14 +34,14 @@ setenv bootargs "root=${rootdev} ro rootwait rootfstype=${rootfstype} ${consolea
 
 if test "${disp_mem_reserves}" = "off"; then setenv bootargs "${bootargs} sunxi_ve_mem_reserve=0 sunxi_g2d_mem_reserve=0 sunxi_fb_mem_reserve=16"; fi
 
-load ${devtype} 0 ${kernel_addr_r} /boot/zImage || load ${devtype} 0 ${kernel_addr_r} zImage
+load ${devtype} 0 ${kernel_addr_r} /boot/zImage
 
-if load ${devtype} 0 0x00000000 /boot/.next || load ${devtype} 0 0x00000000 .next; then
-	load ${devtype} 0 ${fdt_addr_r} /boot/dtb/${fdtfile} || load ${devtype} 0 ${fdt_addr_r} /dtb/${fdtfile}
+if load ${devtype} 0 0x00000000 /boot/.next; then
+	load ${devtype} 0 ${fdt_addr_r} /boot/dtb/${fdtfile}
 	fdt addr ${fdt_addr_r}
 	bootz ${kernel_addr_r} ${fdt_addr_r}
 else
-	load ${devtype} 0 ${fdt_addr_r} /boot/script.bin || load ${devtype} 0 ${fdt_addr_r} script.bin
+	load ${devtype} 0 ${fdt_addr_r} /boot/script.bin
 	bootz ${kernel_addr_r}
 fi
 
