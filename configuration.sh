40219b7f6c9b51eb0689ac9d3b5a809da13a8bb0
mara <mara@fail.pp.ua>
2018-10-22 23:09:42 +0300

    mainline kernel 4.14.y

--- configuration.sh
+++ configuration.sh
@@ -17,7 +17,7 @@ get_config
 if [[ $KERNEL_SOURCE == next ]]; then
     LINUX_SOURCE="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux-stable"
     #LINUX_SOURCE='git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git'
-    KERNEL_BRANCH="linux-4.18.y::"
+    KERNEL_BRANCH="linux-4.14.y::"
     KERNEL_DIR="linux-$KERNEL_SOURCE"
 fi
 
