54af16a133586f20577af187f90f3e06d771ad77
mara <mara@fail.pp.ua>
2019-01-30 19:44:37 +0200

    in boot script added variable rootfstype

--- config/boot_scripts/boot-rk3288.cmd
+++ config/boot_scripts/boot-rk3288.cmd
@@ -8,6 +8,7 @@ setenv rootdev "/dev/mmcblk2p1"
 setenv fdt_file "rk3288-firefly.dtb"
 setenv console "both"
 setenv verbosity "4"
+setenv rootfstype "ext4"
 
 # boot from eMMC
 part uuid mmc 1 part_exists
@@ -27,7 +28,7 @@ fi
 if test "${console}" = "display" || test "${console}" = "both"; then setenv consoleargs "console=%SERIAL_CONSOLE%,%SERIAL_CONSOLE_SPEED%n8"; fi
 if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "${consoleargs} earlyprintk console=tty1"; fi
 
-setenv bootargs "consoleblank=0 scandelay root=${rootdev} ro rootwait rootfstype=ext4 init=/sbin/init ${consoleargs} loglevel=${verbosity} ${extraargs}"
+setenv bootargs "consoleblank=0 scandelay root=${rootdev} ro rootwait rootfstype=${rootfstype} init=/sbin/init ${consoleargs} loglevel=${verbosity} ${extraargs}"
 
 load ${devtype} ${devnum}:1 ${fdt_addr_r} ${prefix}dtb/${fdt_file}
 load ${devtype} ${devnum}:1 ${kernel_addr_r} ${prefix}zImage
