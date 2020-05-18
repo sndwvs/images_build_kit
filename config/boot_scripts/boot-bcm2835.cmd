88cdaf5ebc8db708a25587a1295f65a700f4087a
mara <mara@fail.pp.ua>
2020-05-18 19:36:06 +0300

    u-boot: broadcom: fixed boot.cmd

--- config/boot_scripts/boot-bcm2835.cmd
+++ config/boot_scripts/boot-bcm2835.cmd
@@ -18,11 +18,11 @@ if test "${console}" = "serial" || test "${console}" = "both"; then setenv conso
 
 setenv bootargs "consoleblank=0 root=${rootdev} ro rootwait rootfstype=${rootfstype} init=/sbin/init ${consoleargs} loglevel=${verbosity} ${extraargs}"
 
-load ${devtype} ${devnum}:1 ${fdt_addr_r} ${prefix}dtb/${fdtfile}
 load ${devtype} ${devnum}:1 ${kernel_addr_r} ${prefix}Image
+load ${devtype} ${devnum}:1 ${fdt_addr_r} ${prefix}${fdtfile}
 
-fdt addr ${fdt_addr_r}
-fdt get value bootargs /chosen bootargs
+#fdt addr ${fdt_addr_r}
+#fdt get value bootargs /chosen bootargs
 
 if load ${devtype} ${devnum}:1 ${ramdisk_addr_r} ${prefix}uInitrd; then
     booti ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r};
