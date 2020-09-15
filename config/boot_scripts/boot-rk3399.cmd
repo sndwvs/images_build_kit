e0297e51542e5039a149fbf22d0de6c7962db5ba
mara <mara@fail.pp.ua>
2020-09-15 20:40:56 +0300

    u-boot: rock pi e fixed load address

--- config/boot_scripts/boot-rk3399.cmd
+++ config/boot_scripts/boot-rk3399.cmd
@@ -3,7 +3,7 @@
 # Please edit /boot/uEnv.txt to set supported parameters
 #
 
-setenv load_addr "0x39000000"
+setenv load_addr "0x9000000"
 # default values
 setenv rootdev "/dev/mmcblk0p1"
 setenv verbosity "1"
