3db889e71a272e2ade5d48b1d1bb2f8fbd94aa55
mara <mara@fail.pp.ua>
2020-10-26 14:41:09 +0200

    broadcom: update bootloader blobs

--- blobs/broadcom/update-bootloader.sh
+++ blobs/broadcom/update-bootloader.sh
@@ -6,7 +6,10 @@ NAME=firmware
 git clone --depth=1 https://github.com/raspberrypi/firmware.git /tmp/${NAME} || exit 1
 
 # Bootloader files for Raspberry Pi
-[[ -d $CWD/boot ]] && rm -rf ${CWD}/boot
-mkdir -p ${CWD}/boot
+if [[ -d $CWD/boot ]]; then
+    rm -rf ${CWD}/boot/*.{bin,dat,elf}
+else
+    mkdir -p ${CWD}/boot
+fi
 cp /tmp/${NAME}/boot/{*.dat,*.bin,*.elf} ${CWD}/boot
 [[ -d /tmp/${NAME} ]] && rm -rf /tmp/${NAME}
