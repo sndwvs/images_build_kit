ce3f6e486a8a2b7dd39cd8a232c949f253199868
mara <mara@fail.pp.ua>
2020-12-12 20:21:10 +0200

    u-boot: amlogic meson-sm1 update scripts

--- config/boot_scripts/boot-meson-sm1.cmd
+++ config/boot_scripts/boot-meson-sm1.cmd
@@ -11,26 +11,89 @@ setenv verbosity "1"
 setenv console "both"
 setenv rootfstype "ext4"
 
+
+# odroid c4 legacy kernel values from boot.ini
+
+setenv dtb_loadaddr "0x1000000"
+setenv k_addr "0x1100000"
+setenv loadaddr "0x1B00000"
+setenv initrd_loadaddr "0x4080000"
+
+setenv display_autodetect "true"
+# HDMI Mode
+# Resolution Configuration
+#    Symbol             | Resolution
+# ----------------------+-------------
+#    "480x272p60hz"     | 480x272 Progressive 60Hz
+#    "480x320p60hz"     | 480x320 Progressive 60Hz
+#    "480p60hz"         | 720x480 Progressive 60Hz
+#    "576p50hz"         | 720x576 Progressive 50Hz
+#    "720p60hz"         | 1280x720 Progressive 60Hz
+#    "720p50hz"         | 1280x720 Progressive 50Hz
+#    "1080p60hz"        | 1920x1080 Progressive 60Hz
+#    "1080p50hz"        | 1920x1080 Progressive 50Hz
+#    "1080p30hz"        | 1920x1080 Progressive 30Hz
+#    "1080p24hz"        | 1920x1080 Progressive 24Hz
+#    "1080i60hz"        | 1920x1080 Interlaced 60Hz
+#    "1080i50hz"        | 1920x1080 Interlaced 50Hz
+#    "2160p60hz"        | 3840x2160 Progressive 60Hz
+#    "2160p50hz"        | 3840x2160 Progressive 50Hz
+#    "2160p30hz"        | 3840x2160 Progressive 30Hz
+#    "2160p25hz"        | 3840x2160 Progressive 25Hz
+#    "2160p24hz"        | 3840x2160 Progressive 24Hz
+#    "smpte24hz"        | 3840x2160 Progressive 24Hz SMPTE
+#    "2160p60hz420"     | 3840x2160 Progressive 60Hz YCbCr 4:2:0
+#    "2160p50hz420"     | 3840x2160 Progressive 50Hz YCbCr 4:2:0
+#    "640x480p60hz"     | 640x480 Progressive 60Hz
+#    "800x480p60hz"     | 800x480 Progressive 60Hz
+#    "800x600p60hz"     | 800x600 Progressive 60Hz
+#    "1024x600p60hz"    | 1024x600 Progressive 60Hz
+#    "1024x768p60hz"    | 1024x768 Progressive 60Hz
+#    "1280x800p60hz"    | 1280x800 Progressive 60Hz
+#    "1280x1024p60hz"   | 1280x1024 Progressive 60Hz
+#    "1360x768p60hz"    | 1360x768 Progressive 60Hz
+#    "1440x900p60hz"    | 1440x900 Progressive 60Hz
+#    "1600x900p60hz"    | 1600x900 Progressive 60Hz
+#    "1600x1200p60hz"   | 1600x1200 Progressive 60Hz
+#    "1680x1050p60hz"   | 1680x1050 Progressive 60Hz
+#    "1920x1200p60hz"   | 1920x1200 Progressive 60Hz
+#    "2560x1080p60hz"   | 2560x1080 Progressive 60Hz
+#    "2560x1440p60hz"   | 2560x1440 Progressive 60Hz
+#    "2560x1600p60hz"   | 2560x1600 Progressive 60Hz
+#    "3440x1440p60hz"   | 3440x1440 Progressive 60Hz
+setenv hdmimode "1080p60hz"
+setenv monitor_onoff "false"
+setenv overscan "100"
+setenv sdrmode "auto"
+setenv voutmode "hdmi"
+setenv disablehpd "false"
+setenv cec "false"
+setenv disable_vu7 "true"
+setenv max_freq_a55 "1908"
+#setenv max_freq_a55 "2100"
+setenv maxcpus "4"
+
+# legacy kernel values from boot.ini
+
+
 if test -e ${devtype} ${devnum} ${prefix}uEnv.txt; then
     load ${devtype} ${devnum} ${load_addr} ${prefix}uEnv.txt
     env import -t ${load_addr} ${filesize}
 fi
 
-# get PARTUUID of first partition on SD/eMMC it was loaded from
-# mmc 0 is always mapped to device u-boot (2016.09+) was loaded from
-if test "${devtype}" = "mmc"; then part uuid mmc ${devnum}:1 partuuid; fi
-
 if test "${console}" = "display" || test "${console}" = "both"; then setenv consoleargs "console=%SERIAL_CONSOLE%,%SERIAL_CONSOLE_SPEED%n8"; fi
 if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "${consoleargs} console=tty1"; fi
 
-setenv bootargs "root=${rootdev} ro rootwait rootfstype=${rootfstype} init=/sbin/init ${consoleargs} consoleblank=0 coherent_pool=2M loglevel=${verbosity} ubootpart=${partuuid} usb-storage.quirks=${usbstoragequirks} ${extraargs} ${extraboardargs}"
-
-load ${devtype} ${devnum} ${ramdisk_addr_r} ${prefix}uInitrd
-load ${devtype} ${devnum} ${kernel_addr_r} ${prefix}Image
+setenv bootargs "root=${rootdev} ro rootwait rootfstype=${rootfstype} init=/sbin/init ${consoleargs} consoleblank=0 coherent_pool=2M loglevel=${verbosity} usb-storage.quirks=${usbstoragequirks} ${extraargs} ${extraboardargs}"
 
 load ${devtype} ${devnum} ${fdt_addr_r} ${prefix}dtb/${fdtfile}
+load ${devtype} ${devnum} ${kernel_addr_r} ${prefix}Image
 fdt addr ${fdt_addr_r}
 fdt resize 65536
+
+# read "/chosen" node, property "bootargs", and store in var "dtb_bootargs"
+fdt get value dtb_bootargs /chosen bootargs
+if test "${dtb_bootargs}" != "" ; then setenv bootargs "${bootargs} ${dtb_bootargs}"; fi
 
 if load ${devtype} ${devnum} ${ramdisk_addr_r} ${prefix}uInitrd; then
     booti ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r};
