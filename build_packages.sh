27304b8ac9ffcf1a388189c4f658c2081b27e5ed
mara <mara@fail.pp.ua>
2021-05-30 19:50:23 +0300

    #2 build_packages.sh: fixed clean kernel pre-build dirs

--- build_packages.sh
+++ build_packages.sh
@@ -136,25 +136,25 @@ build_kernel_pkg() {
         cd $BUILD/$PKG/kernel-source/ && mkdir "install"
         cat "$CWD/packages/kernel/slack-desc.kernel-source" | sed "s:%SOCFAMILY%:${SOCFAMILY}:g" > "$BUILD/$PKG/kernel-source/install/slack-desc"
         makepkg -l n -c n $BUILD/$PKG/kernel-source-${SOCFAMILY}-${KERNEL_VERSION}-noarch-${PKG_BUILD}${PACKAGER}.txz >> $LOG 2>&1 || (message "err" "details" && exit 1) || exit 1
+
+        cd $BUILD/$PKG
+
+        # clear kernel packages directories
+        [[ -d "$BUILD/$PKG/kernel-${SOCFAMILY}" ]] && \
+            rm -rf "$BUILD/$PKG/kernel-${SOCFAMILY}" >> $LOG 2>&1 || (message "err" "details" && exit 1) || exit 1
+
+        [[ -d "$BUILD/$PKG/kernel-modules" ]] && \
+            rm -rf "$BUILD/$PKG/kernel-modules" >> $LOG 2>&1 || (message "err" "details" && exit 1) || exit 1
+
+        [[ -d "$BUILD/$PKG/kernel-headers" ]] && \
+            rm -rf "$BUILD/$PKG/kernel-headers" >> $LOG 2>&1 || (message "err" "details" && exit 1) || exit 1
+
+        [[ -d "$BUILD/$PKG/kernel-firmware" ]] && \
+            rm -rf "$BUILD/$PKG/kernel-firmware" >> $LOG 2>&1 || (message "err" "details" && exit 1) || exit 1
+
+        [[ -d "$BUILD/$PKG/kernel-source" ]] && \
+            rm -rf "$BUILD/$PKG/kernel-source" >> $LOG 2>&1 || (message "err" "details" && exit 1) || exit 1
     fi
-
-    cd $BUILD/$PKG
-
-    # clear kernel packages directories
-    [[ -d "$BUILD/$PKG/kernel-${SOCFAMILY}" ]] && \
-        rm -rf "$BUILD/$PKG/kernel-${SOCFAMILY}" >> $LOG 2>&1 || (message "err" "details" && exit 1) || exit 1
-
-    [[ -d "$BUILD/$PKG/kernel-modules" ]] && \
-        rm -rf "$BUILD/$PKG/kernel-modules" >> $LOG 2>&1 || (message "err" "details" && exit 1) || exit 1
-
-    [[ -d "$BUILD/$PKG/kernel-headers" ]] && \
-        rm -rf "$BUILD/$PKG/kernel-headers" >> $LOG 2>&1 || (message "err" "details" && exit 1) || exit 1
-
-    [[ -d "$BUILD/$PKG/kernel-firmware" ]] && \
-        rm -rf "$BUILD/$PKG/kernel-firmware" >> $LOG 2>&1 || (message "err" "details" && exit 1) || exit 1
-
-    [[ -d "$BUILD/$PKG/kernel-source" ]] && \
-        rm -rf "$BUILD/$PKG/kernel-source" >> $LOG 2>&1 || (message "err" "details" && exit 1) || exit 1
 }
 
 
