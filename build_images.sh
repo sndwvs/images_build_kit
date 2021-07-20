3fa479fad30fd14d1360f8b0d887269e6098f48d
mara <mara@fail.pp.ua>
2021-07-21 00:49:10 +0300

    build_images: add mount /boot for bcm2xxx and rk356x

--- build_images.sh
+++ build_images.sh
@@ -44,12 +44,15 @@ setting_fstab() {
     if [[ ! $(cat $SOURCE/$ROOTFS/etc/fstab | grep $ROOT_DISK) ]];then
         message "" "setting" "fstab"
         sed -i "s:# tmpfs:tmpfs:" $SOURCE/$ROOTFS/etc/fstab
-        [[ $SOCFAMILY == bcm2* ]] && echo "/dev/mmcblk0p1    /boot      vfat    defaults       0       1" >> $SOURCE/$ROOTFS/etc/fstab
         if [[ $DISTR == sla* ]]; then
             echo "/dev/$ROOT_DISK    /          ext4    noatime,nodiratime,data=writeback,errors=remount-ro       0       1" >> $SOURCE/$ROOTFS/etc/fstab || exit 1
+            [[ $SOCFAMILY == bcm2* ]] && echo "/dev/mmcblk0p1    /boot      vfat    defaults       0       1" >> $SOURCE/$ROOTFS/etc/fstab
+            [[ $SOCFAMILY == rk356* ]] && echo "/dev/mmcblk0p2    /boot      ext4    noatime,nodiratime       0       1" >> $SOURCE/$ROOTFS/etc/fstab
         elif [[ $DISTR == crux* ]]; then
             sed -i 's:#\(shm\):\1:' $SOURCE/$ROOTFS/etc/fstab || exit 1
             sed -i "/\# End of file/ i \\/dev\/$ROOT_DISK    \/          ext4    noatime,nodiratime,data=writeback,errors=remount-ro       0       1\n" $SOURCE/$ROOTFS/etc/fstab || exit 1
+            [[ $SOCFAMILY == bcm2* ]] && sed -i "/\# End of file/ i \\/dev\/mmcblk0p1    \/boot      vfat    defaults       0       1\n" $SOURCE/$ROOTFS/etc/fstab
+            [[ $SOCFAMILY == rk356* ]] && sed -i "/\# End of file/ i \\/dev\/mmcblk0p2    \/boot      ext4    noatime,nodiratime       0       1\n" >> $SOURCE/$ROOTFS/etc/fstab
         fi
     fi
 }
